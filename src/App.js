f816586ef0f5961f5cb7db146e5d091f0efdf098
Aleks Stamboliyski <aleks.stamboliyski@gmail.com>
2021-10-20 11:51:49 +0300

    feat: add textarea and button handelrs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [text, setText] = useState(localStorage.getItem("note"));
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea value={text} className="textarea is-large" placeholder="Notes..." onChange={(e) => { setText(e.target.value) }} />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" onClick={() => localStorage.setItem("note", text)}>Save</button>
+        <button className="button is-large" onClick={(e) => { setText("") }}>Clear</button>
       </div>
     </div>
   );
